44e80291e742e4334df184569b895c3d923deef1
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2018-01-10 17:42:54 +0700

    Login with Socmed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
+import firebaseui from 'firebaseui';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  setupFirebase(){
+    const config = {
+      apiKey: "AIzaSyCrLu7tBIy6bqtYq4OWEpmHAfPFaWW0Z3c",
+      authDomain: "final-project-miracle.firebaseapp.com",
+      databaseURL: "https://final-project-miracle.firebaseio.com",
+      projectId: "final-project-miracle",
+      storageBucket: "final-project-miracle.appspot.com",
+      messagingSenderId: "517907313039"
+    };
+    const uiConfig = {
+      signInSuccessUrl: 'http://localhost:3000',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID
+      ]
+    };
+    firebase.initializeApp(config);
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container',uiConfig);
+  }
+  checkLogin(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(user){
+      }else{
+      }
+    });
+  }
+  componentWillMount(){
+    this.setupFirebase();
+    this.checkLogin();
+  }
+  render(){
+    return(
+      <div id="firebaseui-auth-container"></div>
+    )
   }
 }
 
-export default App;
+export default App
