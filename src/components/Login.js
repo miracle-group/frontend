65312b614177fc09b9966a5ba29b2c14e2580161
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2018-01-10 20:50:47 +0700

    Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
+import firebaseui from 'firebaseui';
+
+class Login extends Component {
+  setupFirebase(){
+    const config = {
+      apiKey: "AIzaSyCrLu7tBIy6bqtYq4OWEpmHAfPFaWW0Z3c",
+      authDomain: "final-project-miracle.firebaseapp.com",
+      databaseURL: "https://final-project-miracle.firebaseio.com",
+      projectId: "final-project-miracle",
+      storageBucket: "final-project-miracle.appspot.com",
+      messagingSenderId: "517907313039"
+    };
+    const uiConfig = {
+      signInSuccessUrl: 'http://localhost:3000',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID
+      ]
+    };
+    firebase.initializeApp(config);
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container',uiConfig);
+  }
+  checkLogin(){
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if(user){
+      }else{
+      }
+    });
+  }
+  componentWillMount(){
+    this.setupFirebase();
+    this.checkLogin();
+  }
+  render(){
+    return(
+      <div id="firebaseui-auth-container"></div>
+    )
+  }
+}
+
+export default Login
